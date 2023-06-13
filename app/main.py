a7a5fd1205991468995c4b8c169196ac099833b0
Kamtorn Phanwises <k.phanwises@yahoo.co.uk>
2023-06-13 18:28:36 +0100

    create html template

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+BASE_DIR = pathlib.Path(__file__).parent
+# print((BASE_DIR / 'templates').exists())
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / 'templates'))
+# RESTful API
 
-@app.get("/")
-def home_view():
-    return {'hello': 'world'}
+@app.get("/", response_class=HTMLResponse) # http GET -> JSON
+def home_view(request: Request):
+    print(request)
+    return templates.TemplateResponse('home.html', {'request': request, 'abc': 123})
 
-@app.post("/")
+@app.post("/") # http POST
 def home_detail_view():
     return {'hello': 'world'}
